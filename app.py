9f628ca67aed9b6c709f8a46e083ed1d78b2cc49
vaishkadukar <vgkadukar21@gmail.com>
2023-11-30 23:33:18 +0530

    added menu template

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print('Hello world')
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+selector = option_menu(menu_title='Menu Options',options=['Cleaning','Visualisations'],default_index=1,orientation='horizontal')
+with st.container():
+    st.write('hello from header')
+    
